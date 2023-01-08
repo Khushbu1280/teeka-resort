85ce23570edd0bb0057e115943e31945a9ce945a
Khushbu chaulagain <kc.kc.khushbu@gmail.com>
2023-01-08 18:11:21 +0545

    added modal for login

---
+++ script.js
@@ -0,0 +1,28 @@
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+const login = document.getElementById('login');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
+login.addEventListener("click",function(event){
+    fetch('http://localhost:3000/login').then(data => {
+        console.log(data);
+    })
+})
